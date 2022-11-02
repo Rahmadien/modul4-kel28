70754ce1aa5e1a1e3888bd45bfc4eaba8fd49fce
rhmdnakbrzsyflhq@gmail.com <rhmdnakbrzsyflhq@gmail.com>
2022-11-03 04:38:23 +0700

    app.js

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
 import Counter from "./learn-useState/Counter";
 import Effect from "./learn-useEffect/Index";
 import Context from "./learn-useContext/Index";
+import Ref from "./learn-useRef/index";
 
 function App() {
   return (
@@ -23,12 +24,16 @@ function App() {
             <li className="li">
               <Link to="/context">Use Context</Link>
             </li>
+            <li className="li">
+              <Link to="/ref">Use Ref</Link>
+            </li>
           </ul>
         </nav>
         <Routes>
           <Route path='/' exact element = {<Counter/>}/>
           <Route path="/effect" element={<Effect />} />
           <Route path="/context" element={<Context />} />
+          <Route path="/ref" element={<Ref />} />
         </Routes>
       </div>
     </Router>
